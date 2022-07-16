c05cd94083d30449e7625d816f30a4c3dac4154e
Prashant <invokeai.labs@gmail.com>
2022-07-16 18:51:31 +0530

    Updates

---
+++ precache-manifest.c6b923661e0d979c2fe6e391bd966c06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd7fc894d02ac3b4930c846fd1b0b2fe",
+    "url": "/ObjectDetectionApp/index.html"
+  },
+  {
+    "revision": "1aef91036c9671f918ef",
+    "url": "/ObjectDetectionApp/static/css/main.4833ece9.chunk.css"
+  },
+  {
+    "revision": "1cc59cce10219f4c4d9c",
+    "url": "/ObjectDetectionApp/static/js/2.96ac46f7.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/ObjectDetectionApp/static/js/2.96ac46f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aef91036c9671f918ef",
+    "url": "/ObjectDetectionApp/static/js/main.b528be72.chunk.js"
+  },
+  {
+    "revision": "fc36e426b7ec6768c563",
+    "url": "/ObjectDetectionApp/static/js/runtime-main.5250cb7d.js"
+  }
+]);
